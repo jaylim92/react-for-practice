945a0b532073e5d72d84103755033223e4c1223a
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-02-15 21:46:28 +0900

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [keyword, setKeyword] = useState("");
+  const onClick = () => setValue((prev) => prev + 1);
+  const onChange = (event) => setKeyword(event.target.value);
+  const reset = () => setValue(0);
+  console.log("i run all the time");
+  useEffect(() => {
+    console.log("CALL THE API...");
+  }, []);
+  useEffect(() => {
+    if (keyword !== "" && keyword.length > 5) {
+      console.log("Shearch For", keyword);
+    }
+  }, [keyword]);
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text={"Countinue"} />
+      <input
+        value={keyword}
+        onChange={onChange}
+        type="text"
+        placeholder="Search Here"
+      />
+      <h1>{counter}</h1>
+      <button onClick={onClick}>click me</button>
+      <button onClick={reset}>Reset</button>
     </div>
   );
 }

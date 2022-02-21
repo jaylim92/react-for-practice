fe53d477839cd48b39c919828623ae9e49227d1c
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-02-21 21:02:35 +0900

    Create Movie Home page

--- src/App.js
+++ src/App.js
@@ -1,37 +1,5 @@
-import { useEffect, useState } from "react";
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [coins, setCoins] = useState([]);
-  const [converter, setConverter] = useState("");
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => {
-        setCoins(json);
-        setLoading(false);
-      });
-  }, []);
-
-  return (
-    <div>
-      <h1>The Coins! {loading ? "" : `(${coins.length})`}</h1>
-      {loading ? (
-        <strong>Loading...</strong>
-      ) : (
-        <select>
-          {coins.map((coin) => (
-            <option>
-              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD
-            </option>
-          ))}
-        </select>
-      )}
-      <br />
-      <input type="text" placeholder="USD" value={converter}></input>
-      <h3>{converter}</h3>
-    </div>
-  );
+  return null;
 }
 
 export default App;

003cec0cb1adebb3a63004aa61eecbb2d2c0ee86
jaylim92 <90398720+jaylim92@users.noreply.github.com>
2022-02-28 23:45:18 +0900

    set Detail

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
 function Detail() {
-  return <h1>Detail</h1>;
+  const { id } = useParams();
+  const [movie, setMovie] = useState([]);
+  useEffect(() => {
+    const getDetail = async () => {
+      const json = await (
+        await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
+      ).json();
+      setMovie(json.data.movie);
+    };
+    getDetail();
+  }, [id]);
+  console.log(movie);
+  return (
+    <div>
+      {movie.map((movie) => {
+        <h1 key={movie.title}>{movie.title}</h1>;
+      })}
+    </div>
+  );
 }
 
 export default Detail;
